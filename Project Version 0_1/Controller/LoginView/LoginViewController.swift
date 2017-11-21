e1bd46c078ad6659a6b31863f47f9d10c22f063d
erkutbas <ebas1@binghamton.edu>
2017-11-21 23:45:46 +0300

    viewControllers are integrated for the purpose of business flow unit tests

--- Project Version 0_1/Controller/LoginView/LoginViewController.swift
+++ Project Version 0_1/Controller/LoginView/LoginViewController.swift
@@ -22,7 +22,25 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        email.text = "erkutbas007@gmail.com"
+        password.text = "123456"
+        
         disableLoginButton()
+        
+        //testTogoVerificationPanel()
+        
+    }
+    
+    /*
+        test functions
+     */
+    func testTogoVerificationPanel() {
+        
+        let verificationViewControllerObject = storyboard?.instantiateViewController(withIdentifier: "VerificationViewControllerID") as! VerificationViewController
+     
+        verificationViewControllerObject.emailStringValue = "taksi@hotmail.com"
+        
+        navigationController?.pushViewController(verificationViewControllerObject, animated: true)
         
     }
 
@@ -30,14 +48,118 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func loginButtonClick(_ sender: UIButton) {
-                
+
+    /**
+     disable login button
+     */
+    func disableLoginButton() {
         
+        Login.isEnabled = false
         
     }
     
-
+    @IBAction func loginButtonClick(_ sender: UIButton) {
+        
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (user, error) in
+            
+            /*
+                error handling onto firebase ???
+             */
+            if error != nil {
+                
+                /*
+                if let errorMessage = error as NSError? {
+                    
+                    print("LoginButtonClick : \(errorMessage)")
+                    print("LoginButtonClick : \(errorMessage.userInfo)")
+                    print("LoginButtonClick : \(errorMessage.localizedDescription)")
+                    
+                }*/
+                
+                Firebase.AuthErrorCode(rawValue: (error?._code)!)
+                
+                if let errorCode = Firebase.AuthErrorCode(rawValue: (error?._code)!) {
+                    
+                    switch errorCode {
+                        
+                    case .userNotFound:
+                        print("user is not found, let's create one")
+                        self.createUserWithCredentials()
+                        
+                    default:
+                        print("nothing to do at this point")
+                    }
+                }
+            
+                /*
+                    user is already created and logged in process is successfull
+                 */
+            } else {
+                
+                print("loginButtonClick : user is logged in successfully")
+                print("loginButtonClick : user.isEmailVerified : \(user?.isEmailVerified)")
+                // let's check user object is not null
+                if let user = user {
+                    
+                    if user.isEmailVerified == false {
+                        
+                        self.gotoVerificationPanel()
+                        
+                    }
+                    
+                }
+                
+            }
+            
+            
+        }
+    }
+    
+    /*
+        if user mail address is not verified, force the account to get verified.....
+     */
+    func gotoVerificationPanel() {
+        
+        //performSegue(withIdentifier: "gotoVerificationPanel", sender: self)
+        
+        let verificationViewControllerObject = storyboard?.instantiateViewController(withIdentifier: "VerificationViewControllerID") as! VerificationViewController
+        
+        verificationViewControllerObject.emailStringValue = email.text!
+        //verificationViewControllerObject.callerViewControllerID = self.restorationIdentifier!
+        
+        navigationController?.pushViewController(verificationViewControllerObject, animated: true)
+        
+    }
+    
+    func createUserWithCredentials() {
+        
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, error) in
+            
+            if error != nil {
+                print("createUserWithCredentials : Unable to authenticate with Firebase using email")
+                
+                if let errorMessage = error as NSError? {
+                    
+                    print("createUserWithCredentials : errorMessage : \(errorMessage.userInfo)")
+                    print("createUserWithCredentials : errorMessage : \(errorMessage)")
+                    
+                }
+                
+                if let errorCode = Firebase.AuthErrorCode(rawValue: (error?._code)!) {
+                    
+                    print("createUserWithCredentials : ErrorCode : \(errorCode.rawValue)")
+                    
+                }
+                
+                
+            } else {
+                
+                print("createUserWithCredentials : User successfully created in firebase")
+                
+            }
+        }
+    }
+    
     /**
         email & password field must be check before login,
         if email or password is not validated login button must be disabled
@@ -82,16 +204,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             }
             
         }
-        
     }
-    
-    /**
-        disable login button
-     */
-    private func disableLoginButton() {
         
-        Login.isEnabled = false
         
-    }
-
 }
